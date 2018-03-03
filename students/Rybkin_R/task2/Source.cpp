0864a03a7547ab495af1b87a034443a045421cfd
unknown <roma-roma97@mail.ru>
2018-03-03 14:47:15 +0300

    Сейчас должно скомпилироваться

--- students/Rybkin_R/task2/Source.cpp
+++ students/Rybkin_R/task2/Source.cpp
@@ -1,11 +1,12 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #define strt "Событий нет"
 
-#include <stdio.h>
+#include <cstdio>
 #include <iostream>
 #include <math.h>
-#include <string.h>
-#include <locale.h>
+#include <cstring>
+#include <clocale>
+#include <windows.h>
 using namespace std;
 class EventCalendar {
 	int day[30];
@@ -13,37 +14,37 @@ class EventCalendar {
 	int year[30];
 	char **Event;
 public:
-	EventCalendar(char *str, int Num = -1, int y = 0, int m = 0, int d = 0)//
+	EventCalendar(char *str = "Событий нет", int Num = -1, int y = 0, int m = 0, int d = 0)
 	{
-		Event = (char**)malloc(30 * sizeof(char*));
+		Event = new char*[30 * sizeof(char*)];                                      
 		for (int i = 0; i < 30; i++)
 		{
 			year[i] = 0;
 			month[i] = 0;
 			day[i] = 0;
-			Event[i] = (char*)malloc(strlen(strt) + 1);
+			Event[i] = new char[strlen(strt) + 1]; 
 			strcpy(Event[i], strt);
 		}
 		if ((Num > -1) && (Num < 30))
 		{
-			free(Event[Num]);
-			Event[Num] = (char*)malloc(strlen(str) + 1);
+			delete[] Event[Num];
+			Event[Num] = new char[strlen(str) + 1]; 
 			strcpy(Event[Num], str);
 		}
 	}
 	~EventCalendar()
 	{
-		free(Event);
+		delete[] Event; 
 	}
 	EventCalendar& operator=(const EventCalendar& calendar)
 	{
-		for (int i = 0; i < 29; i++)
+		for (int i = 0; i < 30; i++)
 		{
 			day[i] = calendar.day[i];
 			month[i] = calendar.month[i];
 			year[i] = calendar.year[i];
-			free(Event[i]);
-			Event[i] = (char*)malloc(strlen(calendar.Event[i]) + 1);
+			delete[] Event[i];//free(Event[i]);
+			Event[i] = new char[strlen(calendar.Event[i]) + 1];
 			strcpy(Event[i], calendar.Event[i]);
 			return *this;
 		}
@@ -52,13 +53,13 @@ public:
 	{
 		if ((Num > -1) && (Num < 30))
 		{
-			if ((_year > 0 && _year < 2020) && (_month > 0 && _month < 13) && (_day > 0 && _day < 32))
+			if ((_year > 0 && _year < 2021) && (_month > 0 && _month < 13) && (_day > 0 && _day < 32))
 			{
 				year[Num] = _year;
 				month[Num] = _month;
 				day[Num] = _day;
-				free(Event[Num]);
-				Event[Num] = (char*)malloc(strlen(str) + 1);
+				delete[] Event[Num];
+				Event[Num] = new char[strlen(str) + 1];
 				strcpy(Event[Num], str);
 			}
 		}
@@ -71,15 +72,15 @@ public:
 	}
 	void ShiftHelp1(int Num, int _year, int _month, int _day)
 	{
-		if ((_day + day[Num]) > 32)
+		if ((_day + day[Num]) > 31)
 		{
-			month[Num] = month[Num] - 1;
+			month[Num] = month[Num] + 1;
 			day[Num] = (_day + day[Num]) - 31;
 		}
 		else
 			day[Num] = day[Num] + _day;
 
-		if ((_month + month[Num]) > 13)
+		if ((_month + month[Num]) > 12)
 		{
 			year[Num] = year[Num] + 1;
 			month[Num] = (_month + month[Num]) - 12;
@@ -142,21 +143,24 @@ int main()
 {
 	int d, m, y, id, Num;
 	char str[200];
-	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);// Для ввода на Кириллице, если не отображает на русском, нужно поменять шрифт консоли на Lucida Console
+	SetConsoleOutputCP(1251);//
+	setlocale(LC_ALL, "Russian");
 	class EventCalendar calendar;
-	cout << "Введите номер события ";
-	cin >> Num; cout << "\n";
+	cout << "Введите номер события(от 0 до 29) ";
+	cin >> Num; cout << endl;
 	cout << "Введите событие: ";
-	cin >> str; cout << "\n"; // использую sin а не gets, потому что студия не определяет gets
-	cout << "Введите дату события в формате: год месяц день: ";
-	cin >> y >> m >> d; cout << "\n";
+	cin.ignore();
+	cin.getline(str, 200, '\n'); cout << endl;
+	cout << "Введите дату события в формате: год месяц день: "; // если дата будет больше 2020 12 31 то программа не запишет ее.
+	cin >> y >> m >> d; cout << endl;
 	calendar.InputEvent(Num, y, m, d, str);
-	calendar.OutEvent(Num); cout << "\n";
+	calendar.OutEvent(Num); cout << endl;
 	cout << "Введите дату, с которой хотите найти разницу в формате: год месяц число: ";
-	cin >> y >> m >> d; cout << "\n";
+	cin >> y >> m >> d; cout << endl;
 	calendar.Difference(Num, y, m, d);
-	cout << "Введите на сколько хотите сместить дату события и вперед(1) или назад(2): год месяц число направление: ";
-	cin >> y >> m >> d >> id; cout << "\n";
+	cout << "Введите на сколько хотите сместить дату события и вперед(1) или назад(2) в формате: год месяц число направление: ";
+	cin >> y >> m >> d >> id; cout << endl;
 	calendar.ShiftEvent(Num, y, m, d, id);
 	calendar.OutEvent(Num);
 	system("pause");
